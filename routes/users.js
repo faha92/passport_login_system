36ce18a2b543c7d0cf562c8c38748f2a8c97e72c
faha <faha@itu.dk>
2023-04-23 13:33:38 +0200

    validation

--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,32 @@ router.get("/login", (req, res) => res.render("login"));
 
 router.get("/register", (req, res) => res.render("register"));
 
+// register handle 
+
+router.post('/register',(req,res) => {
+const {name, email, password, password2} = req.body; // <-- maybe as const for each field
+
+// validation
+
+let errors = [];
+
+// check required fileds
+
+if(!name || !email || !password){
+errors.push({msg: "please fill in all fields"})};
+
+
+// password match
+if(password!== password2){
+    errors.push({msg: "passwords don't match"})};
+    
+    
+// password length
+
+if(password.length < 8){
+    errors.push({msg: "password should be at least 8 characters"});
+    };
+
+});
+
 module.exports = router;
