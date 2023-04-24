a4507f1572ac7e3f95082b3d004699a126929fc3
faha <faha@itu.dk>
2023-04-24 12:19:45 +0200

    ixc

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
 
-    mongoURI: 'mongodb+srv://faha92:password!@passport.vifhrks.mongodb.net/?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://faha92:Space2Create!@passport.vifhrks.mongodb.net/?retryWrites=true&w=majority'
 }
