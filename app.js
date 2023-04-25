1278b3eac36c0c8983f25065c98814d227a5296c
Fabian <faha@itu.dk>
2023-04-25 14:07:15 +0200

    login handle pt2

--- app.js
+++ app.js
@@ -1,39 +1,30 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
+const passport = require("passport");
 const flash = require("connect-flash");
 const session = require("express-session");
-const passport = require("passport");
-
-// passport config
-require("./config/passport");
 
 const app = express();
 
-//db config
+// Passport Config
+require("./config/passport")(passport);
 
+// DB Config
 const db = require("./config/keys").mongoURI;
 
-//connect to Mongo
-
+// Connect to MongoDB
 mongoose
-  .connect(db, { useNewUrlParser: true })
-  .then(() => console.log("mongodb connected"))
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
-// ejs
-
+// EJS
 app.use(expressLayouts);
 app.set("view engine", "ejs");
 
-// parser
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// parse application/json
-app.use(bodyParser.json());
+// Express body parser
+app.use(express.urlencoded({ extended: true }));
 
 // Express session
 app.use(
@@ -44,8 +35,7 @@ app.use(
   })
 );
 
-// passportjs middleware
-
+// Passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -61,11 +51,9 @@ app.use(function (req, res, next) {
 });
 
 // Routes
-
-app.use("/", require("./routes/index"));
-
-app.use("/users", require("./routes/users"));
+app.use("/", require("./routes/index.js"));
+app.use("/users", require("./routes/users.js"));
 
 const PORT = process.env.PORT || 9000;
 
-app.listen(PORT, console.log(`server started on port ${PORT}`));
+app.listen(PORT, console.log(`Server running on  ${PORT}`));
